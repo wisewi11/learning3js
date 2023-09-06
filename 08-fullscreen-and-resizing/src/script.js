d59f75fd11a1c682af29fdbf741f251b9243f7d1
William Zhou <wzzhou@ucsc.edu>
2023-09-05 18:28:43 -0700

    add: finished 08 fullscreen and resizing

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -22,9 +22,54 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize',()=>
+{
+    //Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //Update camera
+    camera.aspect =  sizes.width/ sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+})
+
+window.addEventListener('dblclick',()=>
+{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+
+    if(!fullscreenElement)
+    {
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else
+    {
+        if(DocumentFragment.exitFullscreen)
+        {
+            document.exitFullscreen()
+
+        }
+        else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen(0)
+        }
+    }
+    
+})
 
 /**
  * Camera
@@ -36,6 +81,7 @@ scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
+controls.enabled = false
 controls.enableDamping = true
 
 /**
@@ -45,6 +91,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+
 
 /**
  * Animate
