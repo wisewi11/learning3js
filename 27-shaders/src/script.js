6ba2abf6d895f400c0a88d6d0574c3ac65e0fcfe
William Zhou <wzzhou@ucsc.edu>
2023-09-05 18:28:45 -0700

    add: halfway done with shaders

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
 
 /**
  * Base
@@ -26,7 +28,11 @@ const textureLoader = new THREE.TextureLoader()
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.RawShaderMaterial({
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader
+
+})
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
