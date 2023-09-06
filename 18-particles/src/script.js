074bc016a4fb50fca2ce121db1e57b86441cffd1
William Zhou <wzzhou@ucsc.edu>
2023-09-05 18:28:44 -0700

    add: finished 18 particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -18,22 +18,57 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('textures/particles/2.png')
 
 /**
  * Particles
  */
 
 //Geometry
-const particlesGeometry = new THREE.SphereBufferGeometry(1,32,32)
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++)
+{
+    positions[i]  = (Math.random() -  0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+    
+
+)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+    
+
+)
 
 //Material
 const particlesMaterial = new THREE.PointsMaterial()
-particlesMaterial.size = 0.02
-particlesMaterial.sizeAttenuatin = true
+particlesMaterial.size = 0.1
+particlesMaterial.sizeAttenuation = true
+// particlesMaterial.color  = new THREE.Color('#ff88cc')
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 //Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
+
+
 
 /**
  * Sizes
@@ -88,8 +123,24 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    //Update particles
+    // particles.rotation.y = elapsedTime
+
+    for(let i =  0; i<count; i++)
+    {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3+0]
+
+        particlesGeometry.attributes.position.array[i3+1] = Math.sin(elapsedTime + x)
+
+
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
+
     // Update controls
     controls.update()
+    // particles.rotation.x-=0.001
 
     // Render
     renderer.render(scene, camera)
